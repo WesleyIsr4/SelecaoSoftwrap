a5f9e3d79a84e63460052dff724e191ca4cbfb2a
WesleyIsr4 <wesleeysrael@gmail.com>
2021-05-07 20:15:03 -0300

    fix bug a routes person

--- Backend/src/Routes/person.ts
+++ Backend/src/Routes/person.ts
@@ -5,6 +5,7 @@ const personRoutes = Router();
 const personController = new PersonController();
 
 personRoutes.get('/', personController.index);
+personRoutes.get('/paginated', personController.paginated);
 personRoutes.get('/:id', personController.show);
 personRoutes.post('/', personController.create);
 personRoutes.put('/:id', personController.update);
